fc213c7cf789bd471d140ef8e663b4fc63ba376f
sjhuang26 <sjhuang26@gmail.com>
2019-06-23 21:33:41 -0400

    Delete unused comment

--- src/widgets/ui.ts
+++ src/widgets/ui.ts
@@ -44,69 +44,6 @@ export function ButtonWidget(
     );
 }
 
-/*export type NavValue = {
-    key: any;
-    name: string;
-    children?: NavLeaf[];
-};
-export type NavLeaf = {
-    key: any;
-    name: string;
-};
-export function PillsWidget(
-    state: ObservableState<any>,
-    values: NavValue[]
-): Widget {
-    state.change.listen(() => {
-        dom.children()
-            .find('a')
-            .removeClass('active');
-        dom.children()
-            .eq(state.val)
-            .find('a')
-            .addClass('active');
-    });
-
-    function NavItemWidget(value: NavValue): Widget {
-        if (value.children) {
-            const dropdown = $(
-                '<a class="nav-link dropdown-toggle data-toggle="dropdown"></a>'
-            ).text(value.name);
-            onMount.listen(() => dropdown.dropdown());
-
-            return DomWidget(
-                container('<li class="nav-item dropdown"></li>')(
-                    dropdown,
-                    container('<div class="dropdown-menu"></div>')(
-                        ...value.children.map(leaf =>
-                            $('<a class="dropdown-item"></a>')
-                                .text(leaf.name)
-                                .click(() => {
-                                    state.val = leaf.key;
-                                    state.change.trigger();
-                                })
-                        )
-                    )
-                )
-            );
-        } else {
-            return DomWidget(
-                container('<li class="nav-item"></li>')(
-                    $('<a class="nav-link"></a>').text(value.name)
-                ).click(() => {
-                    state.val = value.key;
-                    state.change.trigger();
-                })
-            );
-        }
-    }
-
-    const dom = container('<ul class="nav nav-pills"></ul>')(
-        values.map(value => NavItemWidget(value).dom)
-    );
-    return { dom };
-}*/
-
 export type FormValueWidget<T> = Widget & {
     getValue(): any;
     setValue(newVal: T): JQuery;
