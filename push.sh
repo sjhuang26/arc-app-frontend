4bd72c12319672ac78fbd395ef3b3c7822541867
Suhao Jeffrey Huang <sjhuang26@gmail.com>
2019-07-01 16:37:56 -0400

    commit

--- push.sh
+++ push.sh
@@ -3,22 +3,23 @@ set -x
 # dir check
 current_d=${PWD##*/}
 
-if [ "$current_d" != "arc-app-frontend" ]
+if [[ "$current_d" != 'arc-app-frontend' ]]
 then
-    echo "WRONG DIRECTORY!"
+    echo 'WRONG DIRECTORY!'
     exit 1
 fi
 
 # delete
-if [ -d "dist" ]; then rm -ri ./dist || exit 1; fi
+if [[ -d 'dist' ]]; then rm -r ./dist || exit 1; fi
 
-# build
-yarn run parcel-build || exit 1
+# build (NOTE: without sourcemaps)
+yarn run parcel-build-nomap || exit 1
 
 # copy files from ./dist to ./arc-app-frontend-server/dist (which is a nested git repository)
 cp ./dist/src.*.js ./arc-app-frontend-server/dist/index.js || exit 1
 cp ./dist/src.*.css ./arc-app-frontend-server/dist/index.css || exit 1
 
+# now we are in the subdirectory!
 cd arc-app-frontend-server || exit 1
 
 # push to git
@@ -26,7 +27,16 @@ cd arc-app-frontend-server || exit 1
 # git commit -m "commit" || exit 1
 # git push || exit 1
 
+# append a footer containing the date, which is useful for debug
+# this will mess up sourcemaps, but sourcemaps aren't being deployed
+{ printf '\n\n\n'; echo '/* Automatically built on '"$(date +"%Y-%m-%d %T")"' */'; printf '\n'; } >> dist/index.js
+
 # deploy to netlify
-netlify deploy -d dist --prod --message "automatic" || exit 1
+netlify deploy -d dist --prod --message 'automatic' || exit 1
 
 cd ..
+
+# all done!
+echo "ALL DONE!"
+echo "JS LINK = https://arc-app-frontend-server.netlify.com/index.js"
+echo "CSS LINK = https://arc-app-frontend-server.netlify.com/index.css"
