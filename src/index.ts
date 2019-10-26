1ea80baee1254c0a7d0fa6b5af365dfb9254aa9b
Suhao Jeffrey Huang <sjhuang26@gmail.com>
2019-10-26 10:23:58 -0400

    w

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
-import { forceRefreshAllResources } from "./core/shared"
 import { rootWidget } from "./core/widget"
+import { forceRefreshAllResources } from "./core/shared"
 
 console.log("hi there!")
+
+const spinnerHTML = `
+<div style="display: flex;align-items: center;justify-content: center;flex-direction: column;">
+<div style="border: 3px solid #DDD; padding: 2rem; border-radius: 1rem; background: #FAFAFA; display: flex;align-items: center;justify-content: center;flex-direction: column;">
+<p class="lead">ARC App</p>
+<div class="spinner-border" style="width: 8rem; height: 8rem" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+</div>
+</div>
+`
+
 window["appOnReady"] = async () => {
-  // TODO: replace with proper loading widget
-  $("body").append($('<h1 id="app">Loading...</h1>'))
+  $("#app")
+    .addClass("layout-v")
+    .append(spinnerHTML)
   await forceRefreshAllResources()
   $("body").empty()
   $("body").append(rootWidget().dom)
